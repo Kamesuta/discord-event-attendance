1bf67840e29499e2e11a78218bac3c4176303b51
Kamesuta <kamesuta@gmail.com>
2024-06-16 11:01:19 +0900

    コマンドでメッセージを更新できるように

---
+++ src/commands/event_admin_command/EventAdminUpdateMessageCommand.ts
@@ -0,0 +1,89 @@
+import {
+  ChatInputCommandInteraction,
+  SlashCommandSubcommandBuilder,
+} from 'discord.js';
+import { SubcommandInteraction } from '../base/command_base.js';
+import eventManager from '../../event/EventManager.js';
+import { updateEvent } from '../../event_handler.js';
+import eventAdminCommand from './EventAdminCommand.js';
+import showEvent from '../../event/showEvent.js';
+
+class EventAdminUpdateMessageCommand extends SubcommandInteraction {
+  command = new SlashCommandSubcommandBuilder()
+    .setName('update_message')
+    .setDescription('イベント情報を更新')
+    .addStringOption((option) =>
+      option
+        .setName('message')
+        .setDescription('イベントのメッセージ')
+        .setRequired(true),
+    );
+
+  async onCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+    await interaction.deferReply({ ephemeral: true });
+
+    // メッセージを取得
+    const messageId = interaction.options.getString('message');
+    if (!messageId) return;
+    const message = await interaction.channel?.messages.fetch(messageId);
+    if (!message) {
+      await interaction.editReply({
+        content: 'メッセージが見つかりませんでした',
+      });
+      return;
+    }
+
+    // EmbedのURLを解析
+    const url = message.embeds[0]?.url;
+    if (!url) {
+      await interaction.editReply({
+        content: 'イベントお知らせメッセージに対してのみ使用できます',
+      });
+      return;
+    }
+    const match = url.match(/\/(\d+)$/);
+    if (!match) {
+      await interaction.editReply({
+        content: 'イベントお知らせメッセージのURLが不正です',
+      });
+      return;
+    }
+    const scheduledEventId = match[1];
+    // ScheduledEventが取得できれば更新
+    const scheduledEvent = await interaction.guild?.scheduledEvents
+      .fetch(scheduledEventId)
+      .catch(() => undefined);
+    if (scheduledEvent) {
+      await updateEvent(scheduledEvent);
+    }
+    // イベント情報を取得
+    const event = await eventManager.getEventFromDiscordId(scheduledEventId);
+    if (!event) {
+      await interaction.editReply({
+        content: 'イベントが見つかりませんでした',
+      });
+      return;
+    }
+
+    // メッセージを抽出 (\n\n[～](https://discord.com/events/～) は削除)
+    const messageMatch = message.content.match(
+      /^(.+)(?:\n\n\[(.+)\]\(https:\/\/discord.com\/events\/.+\))?$/,
+    );
+
+    // イベント情報を編集
+    await showEvent(
+      interaction,
+      event,
+      true,
+      messageMatch?.[1],
+      messageMatch?.[2],
+      message,
+    );
+
+    await interaction.editReply({
+      content: `イベント「${event.name}」(ID: ${event.id})の情報を更新しました`,
+    });
+  }
+}
+
+export default new EventAdminUpdateMessageCommand(eventAdminCommand);
--- src/commands/event_admin_command/commands.ts
+++ src/commands/event_admin_command/commands.ts
@@ -2,6 +2,7 @@ import { InteractionBase } from '../base/interaction_base.js';
 import eventAdminCommand from './EventAdminCommand.js';
 import eventAdminSelectCommand from './EventAdminSelectCommand.js';
 import eventAdminUpdateCommand from './EventAdminUpdateCommand.js';
+import eventAdminUpdateMessageCommand from './EventAdminUpdateMessageCommand.js';
 import eventAdminStartCommand from './EventAdminStartCommand.js';
 import eventAdminStopCommand from './EventAdminStopCommand.js';
 
@@ -9,6 +10,7 @@ const commands: InteractionBase[] = [
   eventAdminCommand,
   eventAdminSelectCommand,
   eventAdminUpdateCommand,
+  eventAdminUpdateMessageCommand,
   eventAdminStartCommand,
   eventAdminStopCommand,
 ];
