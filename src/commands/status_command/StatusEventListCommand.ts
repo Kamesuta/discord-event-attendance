a0164dc14061632ee9f651625727c763b6e647e3
Kamesuta <kamesuta@gmail.com>
2024-09-28 10:05:32 +0900

    期間を詳細に指定可能に

--- src/commands/status_command/StatusEventListCommand.ts
+++ src/commands/status_command/StatusEventListCommand.ts
@@ -21,10 +21,12 @@ class StatusEventListCommand extends SubcommandInteraction {
         )
         .setRequired(false),
     )
-    .addIntegerOption((option) =>
+    .addStringOption((option) =>
       option
-        .setName('month')
-        .setDescription('表示する月 (デフォルトは直近30日間を表示します)')
+        .setName('period')
+        .setDescription(
+          '表示する月 (ハイフンで範囲指定可: 「3-5」 = 3月〜5月、スラッシュで年/日指定可: 「2023/3」 = 2023年3月, 「8/5」 = 今年の8月5日)',
+        )
         .setRequired(false),
     )
     .addStringOption((option) =>
@@ -39,21 +41,112 @@ class StatusEventListCommand extends SubcommandInteraction {
     const show = interaction.options.getBoolean('show') ?? false;
     await interaction.deferReply({ ephemeral: !show });
 
-    // 月
-    const month = interaction.options.getInteger('month');
+    // 期間指定
     const currentYear = new Date().getFullYear();
-    const startTime = month
-      ? {
-          gt: new Date(currentYear, month - 1, 1), // 月初め
-          lt: new Date(currentYear, month, 1), // 翌月初め
+    const periodOption = interaction.options.getString('period');
+    let startTime: { gte: Date; lt: Date } | undefined;
+
+    // 期間指定がある場合
+    if (periodOption) {
+      const [start, end] = periodOption.split('-').map((part) => {
+        // 「2024/3/5」→{ year: 2024, month: 3, date: 5 }
+        // 「2024/3」→{ year: 2024, month: 3, date: undefined } (4桁の場合は年指定)
+        // 「8/5」→{ year: currentYear, month: 8, date: 5 }
+        // 「3」→{ year: currentYear, month: 3, date: undefined }
+        // 「2024」→{ year: 2024, month: undefined, date: undefined }
+        const split = part.split('/').map((v) => parseInt(v, 10));
+
+        if (split.length === 3) {
+          // 「2024/3/5」→{ year: 2024, month: 3, date: 5 }
+          return { year: split[0], month: split[1], date: split[2] };
+        } else if (split.length === 2) {
+          if (split[0] > 999) {
+            // 「2024/3」→{ year: 2024, month: 3, date: undefined }
+            return { year: split[0], month: split[1], date: undefined };
+          } else {
+            // 「8/5」→{ year: currentYear, month: 8, date: 5 }
+            return {
+              year: currentYear,
+              month: split[0],
+              date: split[1],
+            };
+          }
+        } else if (split.length === 1) {
+          if (split[0] > 999) {
+            // 「2024」→{ year: 2024, month: undefined, date: undefined }
+            return { year: split[0], month: undefined, date: undefined };
+          } else {
+            // 「3」→{ year: currentYear, month: 3, date: undefined }
+            return {
+              year: currentYear,
+              month: split[0],
+              date: undefined,
+            };
+          }
+        } else {
+          // 不正な入力の場合、undefinedを返す
+          return undefined;
         }
-      : {
-          // 直近1ヶ月
-          gt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
+      });
+
+      if (!start) {
+        // 不正な入力の場合、全期間とする
+        startTime = undefined;
+      } else if (!end) {
+        // 単一指定
+        if (!start.month) {
+          // 年指定
+          startTime = {
+            gte: new Date(start.year, 0, 1), // 年初め
+            lt: new Date(start.year + 1, 0, 1), // 翌年初め
+          };
+        } else if (!start.date) {
+          // 月指定
+          startTime = {
+            gte: new Date(start.year, start.month - 1, 1), // 月初め
+            lt: new Date(start.year, start.month, 1), // 翌月初め
+          };
+        } else {
+          // 日指定
+          startTime = {
+            gte: new Date(start.year, start.month - 1, start.date), // 日初め
+            lt: new Date(start.year, start.month - 1, start.date + 1), // 翌日初め
+          };
+        }
+      } else {
+        // 範囲指定
+        let gte, lt: Date | undefined;
+        if (!start.month) {
+          // 年指定
+          gte = new Date(start.year, 0, 1); // 年初め
+        } else if (!start.date) {
+          // 月指定
+          gte = new Date(start.year, start.month - 1, 1); // 月初め
+        } else {
+          // 日指定
+          gte = new Date(start.year, start.month - 1, start.date); // 日初め
+        }
+        if (!end.month) {
+          // 年指定
+          lt = new Date(end.year + 1, 0, 1); // 翌年初め
+        } else if (!end.date) {
+          // 月指定
+          lt = new Date(end.year, end.month, 1); // 翌月初め
+        } else {
+          // 日指定
+          lt = new Date(end.year, end.month - 1, end.date); // 翌日初め
+        }
+        startTime = {
+          gte,
+          lt,
         };
-    const piriodText = month
-      ? `${month}月`
-      : `直近30日間 (<t:${Math.floor(startTime.gt.getTime() / 1000)}:D> 〜 <t:${Math.floor(startTime.lt?.getTime() ?? Date.now() / 1000)}:D>)`;
+      }
+    }
+
+    // 期間テキスト
+    const periodText = startTime
+      ? `<t:${Math.floor(startTime.gte.getTime() / 1000)}:D> 〜 <t:${Math.floor(startTime.lt.getTime() / 1000)}:D>`
+      : '全期間';
 
     // 検索条件
     const search = interaction.options.getString('search');
@@ -72,7 +165,7 @@ class StatusEventListCommand extends SubcommandInteraction {
 
     // Embed作成
     const embeds = new EmbedBuilder()
-      .setTitle(`イベント一覧 (${piriodText}, ${eventList.length}件)`)
+      .setTitle(`イベント一覧 (${periodText}, ${eventList.length}件)`)
       .setDescription(eventList.join('\n') || 'イベントがありません')
       .setColor('#ff8c00')
       .setFooter({
